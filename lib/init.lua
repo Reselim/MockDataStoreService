cd13c0578ac31004f1ea9721448e1b054607d121
Reselim <notreselim@gmail.com>
2021-01-18 19:37:06 -0500

    Remove yielding check for API access

--- lib/init.lua
+++ lib/init.lua
@@ -8,22 +8,8 @@
 
 local MockDataStoreServiceModule = script.MockDataStoreService
 
-local shouldUseMock = false
-if game.GameId == 0 then -- Local place file
-	shouldUseMock = true
-elseif game:GetService("RunService"):IsStudio() then -- Published file in Studio
-	local status, message = pcall(function()
-		-- This will error if current instance has no Studio API access:
-		game:GetService("DataStoreService"):GetDataStore("__TEST"):SetAsync("__TEST", "__TEST_" .. os.time())
-	end)
-	if not status and message:find("403", 1, true) then -- HACK
-		-- Can connect to datastores, but no API access
-		shouldUseMock = true
-	end
-end
-
 -- Return the mock or actual service depending on environment:
-if shouldUseMock then
+if game.PlaceId == 0 then
 	warn("INFO: Using MockDataStoreService instead of DataStoreService")
 	return require(MockDataStoreServiceModule)
 else
